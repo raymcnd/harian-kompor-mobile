99303c1d760a8754e07603bf484460d50ecac0ae
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-07-25 16:22:23 +0700

    chore: setup deployment

--- server/orchestrator/schema/PostSchema.js
+++ server/orchestrator/schema/PostSchema.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
-
-const appBaseUrl = "http://localhost:4002/pub"
-const usersBaseUrl = "http://localhost:4001"
+const redis = require('../config/redisConfig')
+const appBaseUrl = "http://app-service-container:4002/pub"
+const usersBaseUrl = "http://users-service-container:4001"
 
 const typeDefs = `#graphql
     type Post {
